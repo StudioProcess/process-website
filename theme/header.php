00febf6c8e69e96c3d6219e9df9397f577a252dc
MORITZ RESL <moritz@resl.xyz>
2015-11-04 21:30:25 +0100

    removed template logo

--- theme/header.php
+++ theme/header.php
@@ -31,13 +31,6 @@
 			<!-- header -->
 			<header class="header clear" role="banner">
 				<div class="header-content">
-					<!-- logo -->
-					<!-- <div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/_/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div> -->
-					<!-- /logo -->
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
