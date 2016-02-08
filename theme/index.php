730d5459041567759a2a5b4fd86cd19dd75f4c53
RESL <moritz@resl.xyz>
2016-02-08 17:54:05 +0100

    tag display
    removed social posts & header image when tags are displayed

--- theme/index.php
+++ theme/index.php
@@ -3,10 +3,7 @@
 	<main role="main">
 		<!-- section -->
 		<section>
-
-			<?php
-				include 'card-featured.php';
-			?>
+				<?php if(is_home()) include 'card-featured.php'; ?>
 
 			<?php //PrcsSync::sync(); // TODO: this takes too long
 			$social_posts = prcs_get_social_posts(40);
@@ -17,11 +14,13 @@
 			if (have_posts()): while (have_posts()) : the_post();
 
 				// insert social posts if appropriate
-				while ( prcs_should_insert_social($idx++, $num_social, $num_works) ) {
-					$prcs = current($social_posts);
-					if ($prcs->service == 'twitter') include( locate_template('card-twitter.php') );
-					else if ($prcs->service == 'instagram') include( locate_template('card-instagram.php') );
-					next($social_posts); // advance pointer
+				if( is_home() ){
+					while ( prcs_should_insert_social($idx++, $num_social, $num_works) ) {
+						$prcs = current($social_posts);
+						if ($prcs->service == 'twitter') include( locate_template('card-twitter.php') );
+						else if ($prcs->service == 'instagram') include( locate_template('card-instagram.php') );
+						next($social_posts); // advance pointer
+					}
 				}
 
 				// insert next work
