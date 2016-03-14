57da8747eccece8d00a799783d563edfd63601b2
RESL <moritz@resl.xyz>
2016-03-14 20:50:53 +0100

    featured info

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,3 +1,8 @@
 <article class="featured">
-   <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;">
+   <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
+   <strong>Process is a studio for Art and Design.</strong><br />
+   This is a stream of visuals artifacts, tweets and finished projects.<br />
+   Click &#8594; <a href="/works/" style="text-decoration: underline;">this link</a> to see finished works only.<br />
+   More about us &#8594; <a href="/about/" style="text-decoration: underline;">here</a>.
+   <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
