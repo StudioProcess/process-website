ebb70be6a1299509126f815f826237f5bd87322b
RESL <moritz@resl.xyz>
2016-03-14 21:18:08 +0100

    update mobile version card featured

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,8 +1,8 @@
 <article class="featured">
    <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
-   <strong>Process is a studio for Art and Design.</strong><br />
-   This is a stream of visuals artifacts, tweets and finished projects.<br />
-   Click &#8594; <a href="/works/" style="text-decoration: underline;">this link</a> to see finished works only.<br />
-   More about us &#8594; <a href="/about/" style="text-decoration: underline;">here</a>.
+   <strong>Process — Studio for Art and Design</strong><br />
+   This is a stream of instagram images, tweets and finished projects.<br />
+   Click &#8594; <a href="/works/" style="text-decoration: underline;">this</a> for finished works only.<br />
+   More information about us &#8594; <a href="/about/" style="text-decoration: underline;">here</a>.
    <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
