35947ef4984314473e875d85eacc13d158acf390
RESL <moritz@resl.xyz>
2016-03-15 10:02:57 +0100

    featured card

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -2,7 +2,7 @@
    <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
    <strong>Process — Studio for Art and Design</strong><br />
    This is a stream of instagram images, tweets and finished projects.<br />
-   Click &#8594; <a href="/works/" style="text-decoration: underline;">this</a> for finished works only.<br />
-   More information about us &#8594; <a href="/about/" style="text-decoration: underline;">here</a>.
+
+   View finished &#8594; <a href="/works/" style="text-decoration: underline;">works</a> only and more information &#8594; <a href="/about/" style="text-decoration: underline;">about us</a>.
    <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
