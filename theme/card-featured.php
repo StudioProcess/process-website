5db6d9f34df22e24175840c1cb53af3b0dd289e3
RESL <moritz@resl.xyz>
2016-03-15 11:03:33 +0100

    card featured @media

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -3,6 +3,6 @@
    <strong>Process — Studio for Art and Design</strong><br />
    This is a stream of instagram images, tweets and finished projects.<br />
 
-   View finished &#8594; <a href="/works/" style="text-decoration: underline;">works</a> only and more information &#8594; <a href="/about/" style="text-decoration: underline;">about us</a>.
+   View finished &#8594; <a href="/works/">works</a> only and more information &#8594; <a href="/about/">about us</a>.
    <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
