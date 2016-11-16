7c5ec1d87cda463b01943afe2fa13761b2a9a31a
RESL <moritz@resl.xyz>
2016-11-16 11:00:20 +0100

    embed pace script without 'www'

--- theme/header.php
+++ theme/header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<script src="http://www.process.studio/wp-content/themes/process2015/pace.min.js"></script>
+		<script src="http://process.studio/wp-content/themes/process2015/pace.min.js"></script>
 
 		<?php wp_head(); ?>
 
