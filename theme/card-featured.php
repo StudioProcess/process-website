6ac554ac23f7bfeb2ec7c61ef17e8616656b5479
RESL <moritz@resl.xyz>
2016-11-25 00:27:22 +0100

    startpage: two columns featured image

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,13 +1,24 @@
-<article class="featured-image">
+<!-- <article class="featured-image">
    <img src="https://c2.staticflickr.com/4/3696/19477955629_1bf5931271_h.jpg">
 </article>
 
 <article class="featured-text">
-   <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
    <p class="big-text-start">
      Process is a design studio based in Vienna, specializing in generative and interactive design and working in the fields of branding, web, installation and print. As well as traditional graphic design solutions Process creates and develops highly specialized software that is used as tools for and by clients.
    </p>
- <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
+</article> -->
+
+<article>
+  <div class="items">
+    <div class="item">
+      <img src="https://c2.staticflickr.com/4/3696/19477955629_1bf5931271_h.jpg">
+    </div>
+    <div class="item">
+      <p class="big-text-start">
+        Process is a design studio based in Vienna, specializing in generative and interactive design and working in the fields of branding, web, installation and print. As well as traditional graphic design solutions Process creates and develops highly specialized software that is used as tools for and by clients.
+      </p>
+    </div>
+  </div>
 </article>
 
 <article class="featured-stream">
