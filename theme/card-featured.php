843632300635107dda6013d4e151222ee1b5fa10
RESL <moritz@resl.xyz>
2016-11-24 17:12:58 +0100

    startpage revision

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -1,8 +1,14 @@
-<article class="featured">
-   <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
-   <strong>Process — Studio for Art and Design</strong><br />
-   This is a stream of instagram images, tweets and finished projects.<br />
+<article class="featured-image">
+   <img src="https://c2.staticflickr.com/4/3696/19477955629_1bf5931271_h.jpg">
+</article>
 
-   View finished &#8594; <a href="/works/">Works</a> only and more information about the &#8594; <a href="/about/">Studio</a>.
+<article class="featured-text">
+   <!-- <img src="/wp-content/uploads/2016/03/ppp.png" style="max-height:500px; width:auto; max-width:100%;"><br /><br /> -->
+   Process is a design studio based in Vienna, specializing in generative and interactive design and working in the fields of branding, web, installation and print. As well as traditional graphic design solutions they create and develop highly specialized software that is used as a tool for and by the clients.<br /><br />
+ <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
+</article>
+
+<article class="featured-stream">
+  This is a stream of instagram images, tweets and finished projects. View finished &#8594; <a href="/works/">Works</a> only and more information about the &#8594; <a href="/about/">Studio</a>.
    <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
