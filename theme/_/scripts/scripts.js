bbbfc8454b37b9be2a8c3dc8a4780c348442a175
Martin Leopold <m@martinleopold.com>
2016-11-25 12:23:14 +0100

    Automatic effect on special words

--- theme/_/scripts/scripts.js
+++ theme/_/scripts/scripts.js
@@ -19,7 +19,8 @@
 		$('body.single-works #menu-item-8').addClass('current-menu-item');
 	});
 	
-	// intro text effect
+	
+	// Intro text effect
 	$(function () {
 		var $block = $('.big-text-start');
 		var str = $block.text().trim();
@@ -33,8 +34,47 @@
 			}
 			htm += '</div>&nbsp;';
 		}
-
 		$block.html(htm);
+		// highlight special words
+		var special = ['process', 'design', 'studio', 'vienna', 'generative', 'interactive', 'branding', 'web', 'installation', 'print', 'graphic', 'software', 'tools'];
+		var $words = $block.find('div');
+		$words.each(function() {
+			var $word = $(this);
+			word = $word.text().toLowerCase();
+			if (special.indexOf(word) != -1) $word.addClass('special');
+		});
+		
+		function runWord($el) {
+			var $spans = $el.find('span');
+			var delay = 100;
+			$spans.each(function(i, el) {
+				var $el = $(el);
+				setTimeout(function() { 
+					$el.addClass('hover'); 
+					setTimeout(function() {$el.removeClass('hover')}, delay/3);
+				}, i*delay);
+			});
+		}
+		
+		var blacklisted = '';
+		function triggerRandomSpecialWord() {
+			var $specialWords = $block.find('div.special');
+			var $word = $specialWords.eq( Math.floor(Math.random()*$specialWords.length) );
+			var word = $word.text().toLowerCase();
+			if (word != blacklisted) {
+				runWord($word);
+				blacklisted = word;
+			}
+		}
+		
+		function trigger() {
+			setTimeout(function() {
+				triggerRandomSpecialWord();
+				trigger();
+			}, Math.random() * 5000 );
+		}
+		
+		trigger();
 	});
 
 } ( this, jQuery ));
