388d31508525a2723476f328d51474d373ca6416
RESL <moritz@resl.xyz>
2016-11-25 09:44:48 +0100

    startpage type treatment

--- theme/card-featured.php
+++ theme/card-featured.php
@@ -8,7 +8,7 @@
    </p>
 </article> -->
 
-<article>
+<!-- <article>
   <div class="items">
     <div class="item">
       <img src="https://c2.staticflickr.com/4/3696/19477955629_1bf5931271_h.jpg">
@@ -19,9 +19,14 @@
       </p>
     </div>
   </div>
+</article> -->
+
+<article>
+  <p class="big-text-start">
+    Process is a design studio based in Vienna, specializing in generative and interactive design and working in the fields of branding, web, installation and print. As well as traditional graphic design solutions Process creates and develops highly specialized software that is used as tools for and by clients.
+  </p>
 </article>
 
 <article class="featured-stream">
   This is a stream of instagram images, tweets and finished projects. View finished &#8594; <a href="/works/">Works</a> only and more information about the &#8594; <a href="/about/">Studio</a>.
-   <!-- <img src="/wp-content/uploads/2015/11/preview_fb.png" style="max-height:500px; width:auto; max-width:100%;"> -->
 </article>
