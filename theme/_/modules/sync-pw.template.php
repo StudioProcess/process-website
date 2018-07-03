bc61ad69824748740a5737499b806a0d300bcac0
Martin Leopold <m@martinleopold.com>
2018-07-03 12:42:09 +0200

    Added template for sync-pw.php for completeness' sake

---
+++ theme/_/modules/sync-pw.template.php
@@ -0,0 +1,13 @@
+<?php
+   class PrcsCredentials {
+      const SQL_SERVER = 'localhost';
+      const SQL_DB = '';
+      const SQL_USER = '';
+      const SQL_PASS = '';
+
+      const IG_CLIENT_ID = '';
+      const IG_ACCESS_TOKEN = '';
+      const TWI_KEY = '';
+      const TWI_SECRET = '';
+   }
+?>
