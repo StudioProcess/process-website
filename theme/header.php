67913d3c2765621c1ab62368655c068fed9e3486
Martin Leopold <m@martinleopold.com>
2020-09-03 10:24:15 +0200

    Fix pace.js url

--- theme/header.php
+++ theme/header.php
@@ -11,7 +11,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<script src="//process.studio/wp-content/themes/process2015/pace.min.js"></script>
+		<script src="//process.studio/wp-content/themes/process2015/_/scripts/pace.min.js"></script>
 
 		<?php wp_head(); ?>
 
